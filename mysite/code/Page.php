cbf7c2f3b2a253d7e1d453bec5eae54b63970f82
Garion Herman <garion@silverstripe.com>
2019-08-27 11:03:26 +1200

    Initial commit

---
+++ mysite/code/Page.php
@@ -0,0 +1,14 @@
+<?php
+
+class Page extends SiteTree
+{
+    /**
+     * @var array
+     */
+    private static $db = [];
+
+    /**
+     * @var array
+     */
+    private static $has_one = [];
+}
---
+++ mysite/code/Page_Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+class Page_Controller extends ContentController
+{
+    /**
+     * @var array
+     */
+    private static $allowed_actions = [];
+
+    public function init()
+    {
+        parent::init();
+    }
+}
